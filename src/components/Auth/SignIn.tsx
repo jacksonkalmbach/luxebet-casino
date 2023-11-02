3633b0791aff2ce4978bf42170910736d1288de9
Jackson Kalmbach <jacksonrkalmbach@gmail.com>
2023-11-02 13:54:43 -0500

    sign up with email and password

--- src/components/Auth/SignIn.tsx
+++ src/components/Auth/SignIn.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
@@ -5,11 +6,32 @@ import GoogleIcon from "../../icons/GoogleIcon";
 
 import { setUserLogIn, setUserName } from "../../store/features/user/userSlice";
 import { guestUsernames } from "../../utils/guest/guestUsernames";
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import {
+  signInAuthUserWithEmailAndPassword,
+  signInWithGooglePopup,
+} from "../../utils/firebase/firebase.utils";
+
+type SignInFormType = {
+  email: string;
+  password: string;
+};
+
+const defaultFormFields: SignInFormType = {
+  email: "",
+  password: "",
+};
 
 export default function SignIn({ handleToggle }: { handleToggle: () => void }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [formFields, setFormFields] =
+    useState<SignInFormType>(defaultFormFields);
+  const { email, password } = formFields;
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = event.currentTarget;
+    setFormFields({ ...formFields, [name]: value });
+  };
 
   const handleGuestLogin = () => {
     dispatch(setUserLogIn(true));
@@ -19,6 +41,21 @@ export default function SignIn({ handleToggle }: { handleToggle: () => void }) {
       )
     );
     navigate("/");
+  };
+
+  const handleSubmit = async () => {
+    console.log("sign in");
+    try {
+      const user = await signInAuthUserWithEmailAndPassword(email, password);
+      if (user) {
+        console.log(user);
+        dispatch(setUserLogIn(true));
+        dispatch(setUserName(email));
+        navigate("/");
+      }
+    } catch (error) {
+      console.log("Error signing in with email and password", error);
+    }
   };
 
   const handleGoogleSignIn = async () => {
@@ -37,18 +74,30 @@ export default function SignIn({ handleToggle }: { handleToggle: () => void }) {
   return (
     <div className="flex flex-col md:bg-secondaryBg w-full h-full md:h-2/3 md:w-1/2 rounded-lg p-8 justify-start items-center gap-4 font-oneset md:justify-center">
       <h1 className="text-fontLight text-2xl font-bold">Sign In</h1>
-      <form className="flex flex-col gap-2 w-full md:w-4/5 items-center">
+      <form
+        className="flex flex-col gap-2 w-full md:w-4/5 items-center"
+        onSubmit={handleSubmit}
+      >
         <input
           className="w-full h-10 rounded-lg p-2"
           placeholder="Email Address"
           type="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
         />
         <input
           className="w-full h-10 rounded-lg p-2"
           placeholder="Password"
           type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
         />
-        <button className="w-full h-10 rounded-lg bg-yellow-300 text-black font-bold">
+        <button
+          className="w-full h-10 rounded-lg bg-yellow-300 text-black font-bold"
+          type="submit"
+        >
           Login
         </button>
         <p
--- src/components/Auth/SignUp.tsx
+++ src/components/Auth/SignUp.tsx
@@ -1,15 +1,53 @@
+import { FormEvent, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import GoogleIcon from "../../icons/GoogleIcon";
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import {
+  signInWithGooglePopup,
+  createAuthUserWithEmailAndPassword,
+} from "../../utils/firebase/firebase.utils";
 import { setUserLogIn, setUserName } from "../../store/features/user/userSlice";
+
+type SignUpFormType = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const defaultFormFields: SignUpFormType = {
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 export default function SignUp({ handleToggle }: { handleToggle: () => void }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const handleSubmit = () => {
-    console.log("sign up");
+  const [formFields, setFormFields] =
+    useState<SignUpFormType>(defaultFormFields);
+  const { email, password, confirmPassword } = formFields;
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match.");
+      return;
+    }
+
+    try {
+      const user = await createAuthUserWithEmailAndPassword(email, password);
+      if (user) {
+        dispatch(setUserLogIn(true));
+        dispatch(setUserName(email));
+        navigate("/");
+        setFormFields(defaultFormFields);
+      }
+    } catch (error) {
+      console.log("error creating user", error);
+    }
   };
+
   const handleGoogleSignUp = async () => {
     try {
       const { user } = await signInWithGooglePopup();
@@ -18,8 +56,16 @@ export default function SignUp({ handleToggle }: { handleToggle: () => void }) {
         dispatch(setUserLogIn(true));
         navigate("/");
       }
-    } catch (error) {}
+    } catch (error) {
+      console.log("Error in google sign in", error);
+    }
   };
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = event.currentTarget;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
   return (
     <div className="flex flex-col md:bg-secondaryBg w-full h-full md:h-2/3 md:w-1/2 rounded-lg p-8 justify-start items-center gap-4 font-oneset md:justify-center">
       <h1 className="text-fontLight text-2xl font-bold">Sign Up</h1>
@@ -31,18 +77,30 @@ export default function SignUp({ handleToggle }: { handleToggle: () => void }) {
           className="w-full h-10 rounded-lg p-2"
           placeholder="Email Address"
           type="email"
+          onChange={handleChange}
+          name="email"
+          value={email}
         />
         <input
           className="w-full h-10 rounded-lg p-2"
           placeholder="Password"
           type="password"
+          onChange={handleChange}
+          name="password"
+          value={password}
         />
         <input
           className="w-full h-10 rounded-lg p-2"
           placeholder="Confirm Password"
           type="password"
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
         />
-        <button className="w-full h-10 rounded-lg bg-yellow-300 text-black font-bold">
+        <button
+          className="w-full h-10 rounded-lg bg-yellow-300 text-black font-bold"
+          type="submit"
+        >
           Register
         </button>
       </form>
